e0a97ba09aaf0b73f9d3bddd7d541f50782ddca9
harshit14100 <harshit.arora@remotestate.in>
2026-03-15 23:54:56 +0530

    fixed the card component

--- src/pages/Card.tsx
+++ src/pages/Card.tsx
@@ -10,18 +10,18 @@ interface CardsProps{
 const Card: React.FC<CardsProps> = ({ name, singer, image }) => {
 
   return (
-    <div className='h-35 w-152 flex bg-zinc-600 items-center px-4 rounded-2xl'>
+    <div className='h-40 w-152 max-w-xl flex bg-zinc-700 items-center px-3 rounded-xl hover:bg-zinc-600 transition-all cursor-pointer'>
     <div >
-        <img src={image} className='h-30 w-35 rounded-2xl' />
+        <img src={image} className='h-20 w-25 py-1 rounded-xl object-cover' />
     </div>
     <div className='pl-4 flex justify-between w-full'>
         <div className='flex flex-col gap-1'>
         <h1 className='font-bold text-2xl'>{name}</h1>
         <h4>{singer}</h4>
         </div>
-        <div className='flex  gap-3.5'>
-        <RiPlayListAddFill className='text-3xl'/>
-        <CiHeart className='text-3xl font-bold'/>
+        <div className='flex  gap-3.5 justify-center items-center'>
+        <RiPlayListAddFill className='text-3xl cursor-pointer'/>
+        <CiHeart className='text-3xl font-bold cursor-pointer'/>
         </div>
     </div>
     </div>
